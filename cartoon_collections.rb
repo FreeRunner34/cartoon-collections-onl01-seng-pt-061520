6cc778d8178fcc2c9670bcf2a903747f564508ec
Nikolaus Riggi <nikolaus.riggi@gmail.com>
2020-06-27 04:25:36 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,6 +5,7 @@ end
 
 def summon_captain_planet(planeteer_calls)
   planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
+  return planeteer_calls.map
 end
 
 def long_planeteer_calls# code an argument here
